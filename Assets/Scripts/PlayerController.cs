ef83473baedfe7e21f58d2620afd15015ead626d
Beckudrup <54031477+Beckudrup@users.noreply.github.com>
2023-09-11 14:40:55 +0200

    Camera o Camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,41 +5,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private int playerSpeed = 3;
+    //private int jumpHeight = 5;
 
+    public Transform orientation;
+
+    Rigidbody playerBody;
+    
 
     private PlayerActions playerActions;
 
     private void Awake()
     {
         playerActions = new PlayerActions();
+        playerBody = GetComponent<Rigidbody>();
+        playerBody.freezeRotation = true;
     }
 
     private void OnEnable()
     {
-        playerActions.Enable();
+        //playerActions.Enable();
     }
     private void OnDisable()
     {
-        playerActions.Disable();
+        //playerActions.Disable();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        PlayerMove();
+
+    }
+
+    void PlayerMove()
     {
         Vector3 playerMove = playerActions.Movement.Move.ReadValue<Vector3>();
-        Debug.Log(playerMove); 
+        Debug.Log(playerMove);
+        playerBody.velocity = transform.TransformDirection(playerMove).normalized*playerSpeed;
+        
 
-        if (playerActions.Movement.Jump.triggered)
+        /*if (playerActions.Movement.Jump.triggered)
         {
             Debug.Log("Hop");
             //Hop
         }
+        */
+
     }
 }
